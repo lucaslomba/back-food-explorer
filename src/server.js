7406199fc08c3857bf0126caa33d756440940e7b
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-07 08:42:37 -0300

    Utilizando env no projeto

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+require("dotenv/config")
+
 const express = require("express")
 const app = express()
-const PORT = 3333
 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
