1e3b438274c6be53904e4f85c6205d5852488a08
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-07 08:50:05 -0300

    Base do projeto

--- src/server.js
+++ src/server.js
@@ -2,8 +2,30 @@ require("dotenv/config")
 
 const AppError = require("./utils/AppError")
 
+const cors = require("cors")
 const express = require("express")
+const routes = require("./routes")
+
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
