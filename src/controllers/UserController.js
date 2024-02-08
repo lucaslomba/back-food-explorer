8f15d5f7af73c8872d1463b68acf3a21724bc793
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-08 15:23:11 -0300

    Removendo funcao

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,6 @@ class UsersController {
     async create(request, response){
         console.log(request, response)
     }
-
-    async update(request, response){
-    }
 }
 
 module.exports = UsersController
