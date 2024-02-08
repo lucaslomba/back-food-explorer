1d7fc288891535c18994151b2f87dd80ae68af15
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-08 15:22:39 -0300

    Adicionando Update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,6 +2,10 @@ class UsersController {
     async create(request, response){
         console.log(request, response)
     }
+
+    async update(request, response){
+        console.log(request, response)
+    }
 }
 
 module.exports = UsersController
