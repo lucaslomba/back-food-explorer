6498e843b452a12ee2923ed06516e01963cbc305
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-11 10:33:21 -0300

    Adicionando role na migration

--- src/database/knex/migrations/20240210211908_createUsers.js
+++ src/database/knex/migrations/20240210211908_createUsers.js
@@ -3,6 +3,10 @@ exports.up = knex => knex.schema.createTable("users", table => {
     table.text("name").notNullable()
     table.text("email").notNullable()
     table.text("password").notNullable()
+
+    table
+        .enum("role", ["admin", "customer"], { useNative: true, enumName: "roles" })
+        .notNullable().default("customer")
     
     table.timestamp("created_at").default(knex.fn.now())
     table.timestamp("updated_at").default(knex.fn.now())
