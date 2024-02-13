4d13a5d1e07c8a03a6bdcfeff673a14c086ebf8f
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-12 21:13:57 -0300

    Gerando token e dados de acesso

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,9 @@
+const { compare } = require("bcryptjs")
+const { sign } = require("jsonwebtoken")
+
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
-const { compare } = require("bcryptjs")
+const authConfig = require("../configs/auth")
 
 class SessionsController {
     async create(request, response){
@@ -17,6 +20,14 @@ class SessionsController {
         if(!passwordMatched){
             throw new AppError("E-mail e/ou senha incorreta", 401)
         }
+
+        const { secret, expiresIn } = authConfig.jwt
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+
+        return response.json({ user, token })
     }
 }
 
