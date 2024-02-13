4d59c7f7de1e66521246d3cfd6d433551675cbcc
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-13 19:23:45 -0300

    Refatorando

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,9 @@
 const { Router } = require("express")
 
 const UsersController = require("../controllers/UserController")
-
-const usersRoutes = Router()
-
 const usersController = new UsersController();
 
+const usersRoutes = Router()
 usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
