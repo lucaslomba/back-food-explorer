220ace3d7361beed49ef65d6769d93425f9dc1f1
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-13 19:25:46 -0300

    Adicionando rota ao index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express")
 
 const usersRoutes = require("./users.routes")
+const dishsRoutes = require("./dishs.routes")
 const sessionsRoutes = require("./sessions.routes")
 
 const routes = Router();
 
 routes.use("/users", usersRoutes)
+routes.use("/dishs", dishsRoutes)
 routes.use("/sessions", sessionsRoutes)
 
 module.exports = routes
