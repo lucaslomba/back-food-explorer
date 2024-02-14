74d80cd80bc91cb776bb2e53743077d9bf5e74ab
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-14 10:55:17 -0300

    Rotas autenticadas

--- src/controllers/DishsController.js
+++ src/controllers/DishsController.js
@@ -1,6 +1,12 @@
 class DishsController {
     async create(request, response){
-        
+        const { title, description, tags, links } = request.body
+        const user_id = request.user.id
+
+        console.log(title)
+        console.log(user_id)
+
+        return response.json()
     }
 }
 
--- src/routes/dishs.routes.js
+++ src/routes/dishs.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const DishsController = require("../controllers/DishsController")
 const dishsController = new DishsController()
 
 const dishsRoutes = Router()
+dishsRoutes.use(ensureAuthenticated)
+
 dishsRoutes.post("/", dishsController.create)
 
 module.exports = dishsRoutes
