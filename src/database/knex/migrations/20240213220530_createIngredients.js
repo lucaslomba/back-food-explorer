b0086a7a042a72c6311a6b50c2c93742058c7801
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-16 17:56:22 -0300

    Atualizando criacao de tabela

--- src/database/knex/migrations/20240213220530_createIngredients.js
+++ src/database/knex/migrations/20240213220530_createIngredients.js
@@ -2,7 +2,6 @@ exports.up = knex => knex.schema.createTable("ingredients", table => {
     table.increments("id")
     table.text("name").notNullable()
     table.integer("dish_id").references("id").inTable("dishs").onDelete("CASCADE")
-    table.integer("user_id").references("id").inTable("users")
 })
 
 exports.down = knex => knex.schema.dropTable("ingredients")
