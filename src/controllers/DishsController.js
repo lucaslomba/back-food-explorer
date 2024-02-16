b77618881947925a4e9a038e3e0f197b0f0cb9be
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-16 17:56:35 -0300

    Salvando dados do prato no banco

--- src/controllers/DishsController.js
+++ src/controllers/DishsController.js
@@ -1,10 +1,26 @@
+const knex = require("../database/knex")
+
 class DishsController {
     async create(request, response){
         const { name, category, price, description, ingredients } = request.body
         const user_id = request.user.id
 
-        console.log(name, category, price, description, ingredients)
-        console.log(user_id)
+        const [dish_id] = await knex("dishs").insert({
+            name,
+            category,
+            price,
+            description,
+            user_id
+        })
+
+        const ingredientsInsert = ingredients.map(ingredient => {
+            return {
+                dish_id,
+                name: ingredient
+            }
+        })
+
+        await knex("ingredients").insert(ingredientsInsert)
 
         return response.json()
     }
